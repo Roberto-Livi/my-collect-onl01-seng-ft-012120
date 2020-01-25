8885c4bdfdfac4793b42bbea232bf3c38d819c50
Roberto Livi <robertomlivi@outlook.com>
2020-01-25 16:15:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,22 @@
-def my_collect(gold_members)
-  if gold_members.length == 0
+def my_collect(students, language)
+  if students.length == 0
     puts "There are no members!"
   end
   i = 0
   members_collection = []
-  while gold_members.length > i
-  members_collection << yield(gold_members[i])
+  while students.length > i
+  members_collection << yield(students[i])
   i += 1
   end
   members_collection
+  y = 0
+  upcase_language = []
+  while language.length > y
+    upcase_language << yield(language)
+    y += 1
+    end
+  upcase_language
 end
 
-my_collect(["Peter", "Thomas", "Montclaire", "Kristen"]) {|name| "Welcome to the club #{name}"}
+my_collect(["Tom Jones", "Tom Smith", "Jim Campagno"]) {|name| "Hello, #{name.split(" ".first)}"}
+my_collect(["ruby", "javascript", "python", "objective-c"]) {|l|"#{l.upcase}" }
