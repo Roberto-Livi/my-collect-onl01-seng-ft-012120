148cd4e74ad3152547d73076884c4ea72b504c6b
Roberto Livi <robertomlivi@outlook.com>
2020-01-25 16:06:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(gold_members)
+  if gold_members.length == 0
+    puts "There are no members!"
+  end
+  i = 0
+  members_collection = []
+  while gold_members.length > i
+  members_collection << yield(gold_members[i])
+  i += 1
+  end
+  members_collection
+end
 
-
+my_collect(["Peter", "Thomas", "Montclaire", "Kristen"]) {|name| "Welcome to the club #{name}"}
